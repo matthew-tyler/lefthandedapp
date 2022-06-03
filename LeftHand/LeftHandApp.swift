3f7a325bd02925046b3993d76af94d4fd72413cd
AndrewTrotman <andrew@cs.otago.ac.nz>
2022-06-03 12:35:01 +1200

    Initial Commit

---
+++ LeftHand/LeftHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftHandApp.swift
+//  LeftHand
+//
+//  Created by andrew on 3/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LeftHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
